126091c2864477375a516d95f2c336645a9a4b09
Srdjan Stankovic <srdjan.stankovic@altlabs.dev>
2022-11-06 00:58:14 +0100

    Implement lease renewal on master and chunk server

--- business/core/master/lease_service.go
+++ business/core/master/lease_service.go
@@ -2,9 +2,15 @@ package master
 
 import (
 	"sync"
+	"errors"
 	"time"
 
 	"github.com/google/uuid"
+)
+
+var (
+	ErrLeaseNotFound = errors.New("Lease not found")
+	ErrLeaseNotPreviouslyOwned = errors.New("Failed to extend lease. Chunk Server was not previous owner of the lease")
 )
 
 type Lease struct {
@@ -55,6 +61,21 @@ func (ls *LeaseService) GrantLease(chunkID uuid.UUID, chunkServer *ChunkServerMe
 	return &lease
 }
 
-func (ls *LeaseService) ExtendLease(chunkID ChunkID) (time.Time, error) {
-	return time.Now(), nil
+// ExtendLease extends lease for a given chunkID if chunkserver
+// requesting extension previously had lase over the chunk
+func (ls *LeaseService) ExtendLease(chunkID uuid.UUID, chunkServer *ChunkServerMetadata) (*Lease, error) {
+	prevLease, leaseExists := ls.Leases[chunkID]
+
+	if !leaseExists {
+		return nil, ErrLeaseNotFound
+	}
+
+
+	if prevLease.ChunkServerID != chunkServer.ID {
+		return nil, ErrLeaseNotPreviouslyOwned
+
+	}
+
+	lease := ls.GrantLease(chunkID, chunkServer)
+	return lease, nil
 }
